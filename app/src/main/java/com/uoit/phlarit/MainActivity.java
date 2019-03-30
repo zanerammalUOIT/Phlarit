4c3768c3a095845b4b4feea94c085e90fdeaa64d
zanerammalUOIT <zane.rammal@uoit.net>
2019-03-30 17:17:20 -0400

    Connection work in progress

--- app/src/main/java/com/uoit/phlarit/MainActivity.java
+++ app/src/main/java/com/uoit/phlarit/MainActivity.java
@@ -5,12 +5,21 @@ package com.uoit.phlarit;
  * and the navigation drawer to create a simple UI
  */
 
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.DownloadManager;
 import android.content.ContentProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
@@ -19,12 +28,52 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.apache.commons.io.IOUtils;
+
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.security.SecureRandom;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Date;
+
+import org.json.JSONArray;
+import org.json.simple.parser.JSONParser;
+
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Any fragment used inside the activity must have its
@@ -42,6 +91,12 @@ public class MainActivity extends AppCompatActivity implements HomeFragment.OnFr
     /* Related to the design of navigation drawer*/
     private NavigationView navigationView;
 
+    /*Required to receive JSON from the server*/
+    JSONParser jsonParser = new JSONParser();
+    JSONArray getData = null;
+
+    /* URL to read php script from*/
+    private static final String url_add_data = "http://ec2-54-160-8-114.compute-1.amazonaws.com/db_connect.php";
 
     /* Make all Toasts short*/
     int duration = Toast.LENGTH_SHORT;
@@ -49,11 +104,23 @@ public class MainActivity extends AppCompatActivity implements HomeFragment.OnFr
     /* Declare fragments here so they can be initialized when needed*/
     PhotoFragment photoFragment;
     HomeFragment homeFragment;
+    RequestQueue requestQueue;
+    /* Creating the RequestQueue for http requests*/
+
+    Cache cache;
+    Network network;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        network = new BasicNetwork(new HurlStack());
+        cache = new DiskBasedCache(getCacheDir(), 1024 * 1024 * 150);
+        requestQueue = new RequestQueue(cache, network);
+        checkPermissions(this);
+
+        requestQueue.start();
 
         /* Create instances of the fragments */
         photoFragment = PhotoFragment.newInstance("0", ("0"));
@@ -125,8 +192,12 @@ public class MainActivity extends AppCompatActivity implements HomeFragment.OnFr
 
                 SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yy-hh-mm-ss");
                 String format = simpleDateFormat.format(new Date());
-                Toast toast = Toast.makeText(context, format, duration);
-                toast.show();
+                // Toast toast = Toast.makeText(context, format, duration);
+                // toast.show();
+                new AddRecord().execute(url_add_data);
+
+                // Toast toast1 = Toast.makeText(context, "Execute done", duration);
+                // toast1.show();
                 fragment = photoFragment;
                 break;
 
@@ -191,6 +262,265 @@ public class MainActivity extends AppCompatActivity implements HomeFragment.OnFr
         return super.onOptionsItemSelected(item);
     }
 
-    // -------------------------------
+    /* Will attempt to add to the database in the background once launched*/
+
+    class AddRecord extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        protected String doInBackground(String... args) {
+            /* Use SecureRandom to generate an ID*/
+            SecureRandom random = new SecureRandom();
+            byte bytes[] = new byte[100];
+            random.nextBytes(bytes);
+            Base64.Encoder encoder = Base64.getUrlEncoder().withoutPadding();
+            String token = encoder.encodeToString(bytes);
+
+            String id = token;
+            String image = null; // later set
+            String time;
+
+            /* For testing purposes, convert a Pixabay image into a byte
+             * array and put it in the databse
+             */
+
+            String whaleUrl = "https://cdn.pixabay.com/photo/2013/02/10/00/20/humpback-79855_960_720.jpg";
+
+            try {
+                URL url = new URL(whaleUrl);
+                URLConnection connection = url.openConnection();
+                connection.setConnectTimeout(5000);
+                connection.setReadTimeout(5000);
+                connection.connect();
+
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                IOUtils.copy(connection.getInputStream(), byteArrayOutputStream);
+                image = byteArrayOutputStream.toString();
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yy-hh-mm-ss");
+            String format = simpleDateFormat.format(new Date());
+            time = format;
+
+            List<Pair<String, String>> params = new ArrayList<>();
+            params.add(new Pair<>("id", id));
+            params.add(new Pair<>("image", image));
+            params.add(new Pair<>("time", time));
+
+            final Context context = getApplicationContext();
+
+            /* Running the php script specified*/
+            String phpURL = "http://ec2-54-160-8-114.compute-1.amazonaws.com/db_connect.php";
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, phpURL,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            Log.d("Script", "No error");
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.d("Script", "Error with Volley");
+                        }
+
+                    });
+
+            requestQueue.add(stringRequest);
+            Log.d("queue", "done request queue add");
+
+            String connectURL = "http://ec2-54-160-8-114.compute-1.amazonaws.com/db_connect.php";
+
+            try {
+
+                final HashMap<String, String> paramsSend = new HashMap<>();
+                paramsSend.put("id", id);
+                paramsSend.put("image", image);
+                paramsSend.put("time", time);
+                final Context thisContext = getApplicationContext();
+//
+//                RequestQueue requestQueue = Volley.newRequestQueue(thisContext);
+//                String URL = connectURL;
+//                JSONObject jsonObject = new JSONObject();
+//                jsonObject.put("id", id);
+//                jsonObject.put("image", image);
+//                jsonObject.put("time", time);
+//                final String mRequestBody = jsonObject.toString();
+//
+//                StringRequest stringRequest1 = new StringRequest(Request.Method.POST, URL, new
+//                        Response.Listener<String>() {
+//                            @Override
+//                            public void onResponse(String response) {
+//                                Log.i("LOG_VOLLEY", response);
+//                            }
+//                        }, new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//
+//                    }
+//                }
+//
+//                )
+//
+//
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+//
+                return null;
+            }
+
+//
+        protected void onPostExecute() {
+
+
+        }
+
+    }
+
+    public void checkPermissions(Activity activity) {
+        PackageManager packMan = activity.getPackageManager();
+        int hasWritePermission = packMan.checkPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE, activity.getPackageName());
+        int hasRecordPermission = packMan.checkPermission(Manifest.permission.RECORD_AUDIO, activity.getPackageName());
+        int hasFineLocationPermission = packMan.checkPermission(Manifest.permission.ACCESS_FINE_LOCATION, activity.getPackageName());
+        int hasCoarseLocationPermission = packMan.checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION, activity.getPackageName());
+
+        /* If the API is  greater than 22, we can use runtime permission statements. */
+        if (Build.VERSION.SDK_INT >= 23) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1000);
+            requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO}, 1000);
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1000);
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1000);
+
+        } /* If the API is  greater than 22, we can use runtime permission statements. */
+
+        /* If the API is lower than 23, we cannot use runtime permission statements, so we must check to see if permission has been granted. */
+        if (Build.VERSION.SDK_INT < 23) {
+
+            switch (hasWritePermission) {
+
+                case ((PackageManager.PERMISSION_GRANTED)): {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Write permission is granted.";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                    break;
+                }
+
+                case ((PackageManager.PERMISSION_DENIED)): {
+
+                    Context context = getApplicationContext();
+                    CharSequence text = "Write permission is denied. Application will not function correctly.";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                    break;
+                }
+
+                default:
+
+                    Context context = getApplicationContext();
+                    CharSequence text = "Default statement reached. Application may not function correctly.";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                    break;
+            }
+
+            switch (hasRecordPermission) {
+
+                case ((PackageManager.PERMISSION_GRANTED)): {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Record permission is granted.";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+
+                    break;
+                }
+                case ((PackageManager.PERMISSION_DENIED)): {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Record permission is denied. Application will not function correctly.";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                    break;
+                }
+                default:
+                    // warn that default statement reached
+                    break;
+            }
+
+            switch (hasFineLocationPermission) {
+
+                case ((PackageManager.PERMISSION_GRANTED)): {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Fine Location permission is granted.";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+
+                    break;
+                }
+                case ((PackageManager.PERMISSION_DENIED)): {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Fine Location permission is denied. Application will not function correctly.";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                    break;
+                }
+                default:
+                    // warn that default statement reached
+                    break;
+            }
+
+            switch (hasCoarseLocationPermission) {
+
+                case ((PackageManager.PERMISSION_GRANTED)): {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Coarse Location permission is granted.";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+
+                    break;
+                }
+                case ((PackageManager.PERMISSION_DENIED)): {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Coarse Location permission is denied. Application will not function correctly.";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                    break;
+                }
+                default:
+                    // warn that default statement reached
+                    break;
+            }
+
+
+        } /* If the API is lower than 23, we cannot use runtime permission statements, so we must check to see if permission has been granted. */
+
+
+    }
+
 
 }
